86580ab3bed80611200fe245cd32ddbf18896e62
Erkinai Syrdybaeva <syrdybaeva442.e@gmail.com>
2024-01-12 17:16:34 -0600

    profile added

---
+++ src/Card.jsx
@@ -0,0 +1,17 @@
+
+
+import React from 'react'
+
+const Card = () => {
+    return (
+        <div className='card'>
+            <img src="https://media.licdn.com/dms/image/D5603AQG5Nqi3v3DXyw/profile-displayphoto-shrink_200_200/0/1700616353282?e=1710374400&v=beta&t=gpyOm1CcRBDe63w211Wb9GyqkjW9ww9hb-xbDo6yhOM" alt="Girl pic" />
+            <h2 className='card-title'>Erkesha</h2>
+            <h3 className='card-job'>Web Developer</h3>
+            <p className='card-text'>Feel free to contact me</p>
+            <p className='card-text'>Tel: 7083098286</p>
+        </div>
+    )
+}
+
+export default Card
